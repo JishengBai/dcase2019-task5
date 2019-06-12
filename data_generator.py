dea6ef584e556a224740301d5a4e032301b70c8f
CN-BOTK <40048585+CN-BOTK@users.noreply.github.com>
2019-06-12 20:34:39 +0800

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -15,32 +15,31 @@ import pandas as pd
 def get_train_audiodata(train_label_csv,audio_path):
     name_list = list(train_label_csv['audio_filename'])
     data = []
-    label = np.asarray(train_label_csv)
-    label = list(np.asarray(label[:,1:],dtype=np.float32))
     for name in name_list:
         file_path =os.path.join(audio_path,name)
-        y,sr = librosa.load(file_path,sr = 22050)
+        y,sr = librosa.load(file_path,sr = 32000)
         if len(y)!=sr*10:
             y = np.resize(y,sr*10)
         data.append(y)
-    return np.asarray(data,np.float32),np.asarray(label,np.float32)
+    return np.asarray(data,np.float32)
 
 
 def get_train_data(train_audio_data,feature):
     train_data=[]
     if feature == 'log_mel':  
-        for audio in train_audio_data:
-            data = STFT(audio)
-            data = convert_to_mel_spectrogram(data)
-            data = to_log_mel(data)
-            
-            train_data.append(data)
+        for i in range(data.shape[0]):        
+            stft_matric = librosa.core.stft(train_audio_data[i,:],n_fft=1024,hop_length=500,win_length=1024,window='hann')    
+            mel_W = librosa.filters.mel(sr=32000,n_fft=1024,n_mels=64,fmin=50,fmax=14000)
+            mel_spec = np.dot(mel_W,np.abs(stft_matric)**2)
+            log_mel = librosa.core.power_to_db(mel_spec,top_db=None).T
+            log_mel = log_mel[:-1]
+            train_data.append(log_mel)
     if feature == 'STFT':
         for audio in train_audio_data:
             data = STFT(audio)
             data = power_to_dB(data)
             train_data.append(data)
-    if feature == 'HPSS':        
+    if feature == 'HPSS_h':        
         for audio in train_audio_data:
             data = STFT(audio)
             data_h,data_p = librosa.decompose.hpss(data)
@@ -48,47 +47,27 @@ def get_train_data(train_audio_data,feature):
             train_data.append(data_h)
     return np.asarray(train_data,np.float32)
 
-def divid_data(data,label):
-    divid_data_num = []
-    for i in range(data.shape[0]):
-        if label[i,0]==0 and label[i,4]==0 and label[i,6]==0 :
-            divid_data_num.append(i)
-    print(len(divid_data_num))
-    divid_data_num = np.asarray(divid_data_num)
-    divid_data = data[divid_data_num]
-    divid_label = label[divid_data_num]
-    return divid_data,divid_label
-   
-
-def gen_mel_features(data):
-    train_data = []
-    for i in range(data.shape[0]):        
-        stft_matric = librosa.core.stft(data[i,:],n_fft=1024,hop_length=500,win_length=1024,window='hann')    
-        mel_W = librosa.filters.mel(sr=32000,n_fft=1024,n_mels=64,fmin=50,fmax=14000)
-        mel_spec = np.dot(mel_W,np.abs(stft_matric)**2)
-        log_mel = librosa.core.power_to_db(mel_spec,top_db=None).T
-        log_mel = log_mel[:-1]
-        train_data.append(log_mel)
-    return np.asarray(train_data,dtype=np.float32)
-
-
-def train_divid_class_label():
-    formname = '/home/dcase/c2019/dataset/task5/train_annotations.csv'
-    form = pd.read_csv(formname)
-    col_name = list(form)
-    fine_label = col_name[4:33]
-    coarse_label = col_name[62:]
-    a = form[coarse_label]
-    name = list(form['audio_filename'])
-    namelist = []
-    labellist = []
-    for i in range(0,len(a),3):
-        namelist.append(name[i])
-        d = np.asarray(a)
-        e = d[i:i+3,:]
-        e = np.sum(e,axis=0)
-        e[e<1]=0
-        e[e>=1]=1
         
+ if __name__=='__main__':
+    parameter_dict = load_pars()
+    train_audio_path = parameter_dict['train_audio_path']
+    val_audio_path = parameter_dict['val_audio_path']
+    train_data_path = parameter_dict['train_data_path']
+    val_data_path = parameter_dict['val_data_path']
+    train_label_csv_path = parameter_dict['train_label_csv_path']
+    val_label_csv_path = parameter_dict['val_label_csv_path']
+    feature = parameter_dict['feature_type']
+    
+    train_label_csv = pd.read_csv(train_label_csv_path)
+    val_label_csv = pd.read_csv(val_label_csv_path)
+    train_audio_data = get_train_audiodata(train_label_csv,train_audio_path)
+    val_audio_data = get_train_audiodata(val_label_csv,val_audio_path)
+    train_data = get_train_data(train_audio_data,feature)
+    val_data = get_train_data(val_audio_data,feature)
+    np.save(os.path.join(train_data_path,feature+'.npy'),train_data)
+    np.save(os.path.join(val_data_path,feature+'.npy'),val_data)
+    
+    
+    
     
     
